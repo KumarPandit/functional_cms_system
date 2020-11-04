25bd731cf8d73484d455601cd7c0578dac9eced6
Kumar Prasad Pandit <kumarpndt12@gmail.com>
2020-11-04 12:04:12 +1100

    Code to insert post image

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
                 <?php
                     $query= "SELECT * FROM posts";
                     $select_all_posts_query=mysqli_query($connection, $query);
-                while($row = mysqli_fetch_assoc(@$select_all_posts_query)){
+                while($row = mysqli_fetch_assoc($select_all_posts_query)){
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
@@ -39,7 +39,7 @@
                     </p>
                     <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
                     <hr>
-                    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                    <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
                     <hr>
                     <p><?php echo $post_content ?></p>
                     <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
