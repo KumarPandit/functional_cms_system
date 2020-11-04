2909b9997b3b5e33b8d491b45e8236bfc77f5614
Kumar Prasad Pandit <kumarpndt12@gmail.com>
2020-11-04 16:49:48 +1100

    Dynamic value for category and added sidebar widget

--- includes/navigation.php
+++ includes/navigation.php
@@ -23,7 +23,7 @@
                 <?php
            $query = "SELECT * FROM categories ";
            $select_all_categiories_query = mysqli_query($connection, $query);
-                while($row = mysqli_fetch_assoc(@$select_all_categiories_query)){
+                while($row = mysqli_fetch_assoc($select_all_categiories_query)){
                     $cat_title = $row['cat_title'];
                     echo "<li><a href='#'>{$cat_title}</a></li>";
                 }
