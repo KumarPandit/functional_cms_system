5cfff009f25bc1ddb8a80deabb5dfba4033b4c25
Kumar Prasad Pandit <kumarpndt12@gmail.com>
2020-11-05 15:01:58 +1100

    added code to dynamically get categories from DB

--- admin/categories.php
+++ admin/categories.php
@@ -29,6 +29,12 @@
                     </div>
                     <div class="col-xs-6">
                         <table class="table table-bordered table-hover">
+
+                            <?php
+                            $query = "SELECT * FROM categories ";
+                            $select_categories = mysqli_query($connection, $query);
+                            ?>
+
                             <thead>
                             <tr>
                                 <th>Id</th>
@@ -36,14 +42,16 @@
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td>Baseball Category</td>
-                                <td>Basketball Category</td>
-                            </tr>
-                            <tr>
-                                <td>Baseball Category</td>
-                                <td>Basketball Category</td>
-                            </tr>
+                            <?php
+                            while($row = mysqli_fetch_assoc($select_categories)){
+                                $cat_id = $row['cat_id'];
+                                $cat_title = $row['cat_title'];
+                                echo "<tr>";
+                                echo "<td>{$cat_id}</td>";
+                                echo "<td>{$cat_title}</td>";
+                                echo "</tr>";
+                            }
+                            ?>
                             </tbody>
                         </table>
                     </div>
