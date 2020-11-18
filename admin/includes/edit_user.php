9a7bc43452671911610db0e0088f7edd66e3b22b
Kumar Prasad Pandit <kumarpndt12@gmail.com>
2020-11-19 10:30:00 +1100

    displaying user values in edit page

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -1,30 +1,43 @@
 
 <?php
 //ob_start();
-if(isset($_GET['edit_user'])){
-    echo $the_user_id=$_GET['edit_user'];
+if(isset($_GET['edit_user'])) {
+    $the_user_id = $_GET['edit_user'];
+    $query = "SELECT * FROM users WHERE user_id = $the_user_id ";
+    $select_users_query = mysqli_query($connection, $query);
+    while ($row = mysqli_fetch_assoc($select_users_query)) {
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $user_password = $row['user_password'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_email = $row['user_email'];
+        $user_role = $row['user_role'];
+        $user_image = $row['user_image'];
+
+    }
 }
 
-
-
-if(isset($_POST['edit_user'])){
-
-    $user_firstname = $_POST['user_firstname'];
-    $user_lastname = $_POST['user_lastname'];
-    $user_role = $_POST['user_role'];
-//     $post_image = $_FILES['image']['name'];
-//     $post_image_temp = $_FILES['image']['tmp_name'];
-    $username = $_POST['username'];
-    $user_email = $_POST['user_email'];
-    $user_password = $_POST['user_password'];
-//    $post_date = date('d-m-y');
-
-//    move_uploaded_file($post_image_temp, "../images/$post_image");
-    $query = "INSERT INTO users(user_firstname,user_lastname,user_role,username,user_email,user_password) ";
-    $query .= "VALUES('{$user_firstname}','{$user_lastname}','{$user_role}','{$username}','{$user_email}','{$user_password}' ) ";
-    $insert_user_query = mysqli_query($connection,$query);
-    confirmQuery($insert_user_query);
-}
+//
+//
+//if(isset($_POST['edit_user'])){
+//
+//    $user_firstname = $_POST['user_firstname'];
+//    $user_lastname = $_POST['user_lastname'];
+//    $user_role = $_POST['user_role'];
+////     $post_image = $_FILES['image']['name'];
+////     $post_image_temp = $_FILES['image']['tmp_name'];
+//    $username = $_POST['username'];
+//    $user_email = $_POST['user_email'];
+//    $user_password = $_POST['user_password'];
+////    $post_date = date('d-m-y');
+//
+////    move_uploaded_file($post_image_temp, "../images/$post_image");
+//    $query = "INSERT INTO users(user_firstname,user_lastname,user_role,username,user_email,user_password) ";
+//    $query .= "VALUES('{$user_firstname}','{$user_lastname}','{$user_role}','{$username}','{$user_email}','{$user_password}' ) ";
+//    $insert_user_query = mysqli_query($connection,$query);
+//    confirmQuery($insert_user_query);
+//}
 ?>
 
 
@@ -33,11 +46,11 @@ if(isset($_POST['edit_user'])){
 
     <div class="form-group">
         <label for="title">First Name</label>
-        <input type="text" class="form-control" name="user_firstname">
+        <input type="text" value="<?php echo $user_firstname?>" class="form-control" name="user_firstname">
     </div>
     <div class="form-group">
         <label for="post_status">Last Name</label>
-        <input type="text" class="form-control" name="user_lastname">
+        <input type="text" value="<?php echo $user_lastname; ?>"class="form-control" name="user_lastname">
     </div>
 
     <div class="form-group">
@@ -50,7 +63,7 @@ if(isset($_POST['edit_user'])){
     </div>
     <div class="form-group">
         <label for="title">Username</label>
-        <input type="text" class="form-control" name="username">
+        <input type="text" value="<?php echo $username?>"class="form-control" name="username">
     </div>
     <!--    <div class="form-group">-->
     <!--        <label for="post_image">Post Image</label>-->
@@ -58,11 +71,11 @@ if(isset($_POST['edit_user'])){
     <!--    </div>-->
     <div class="form-group">
         <label for="email">Email</label>
-        <input type="email" class="form-control" name="user_email">
+        <input type="email" value="<?php echo $user_email?>"class="form-control" name="user_email">
     </div>
     <div class="form-group">
         <label for="user_password">Password</label>
-        <input type="password" class="form-control" name="user_password">
+        <input type="password" value="<?php echo $user_password;?>" class="form-control" name="user_password">
     </div>
     <div class="form-group">
         <input type="submit" class="btn btn-primary" name="edit_user" value="Edit User">
