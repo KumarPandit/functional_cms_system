27935e6ce861ba84ee64ea86420ae127317478e5
Kumar Prasad Pandit <kumarpndt12@gmail.com>
2020-11-19 11:47:26 +1100

    created login form

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,6 +18,24 @@
         </form>
     </div>
 
+
+    <!-- Login -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action ="includes/login.php"
+              method="post">
+
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder="Enter Username">
+                <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                <span class-"input-group-btn">
+                <button class ="btn btn-primary" name="login" type="submit">Submit</button></span>
+            </div>
+            <!-- /.input-group -->
+        </form>
+    </div>
+
+
     <!-- Blog Categories Well -->
     <div class="well">
 
